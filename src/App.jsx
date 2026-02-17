cf1b6278f77c963b40a9ba991cb9d92842aea16d
hugoalcala <hugo.pascual.alcala@gmail.com>
2026-02-17 11:31:00 +0100

    ambos modos ya hechos, a lo mejor se cambia algun color en un futuro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from './hooks/useTheme'
 import Home from './pages/Home'
 
 function App() {
-  return <Home />
+  return (
+    <ThemeProvider>
+      <Home />
+    </ThemeProvider>
+  )
 }
 
 export default App
