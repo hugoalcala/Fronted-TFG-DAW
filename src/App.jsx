66e194e49b2a627f9d45a4597053b2463f1a681a
hugoalcala <hugo.pascual.alcala@gmail.com>
2026-02-17 14:13:46 +0100

    creando el login

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './hooks/useTheme'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <ThemeProvider>
-      <Home />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
 
